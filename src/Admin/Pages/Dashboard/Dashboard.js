c4ef547806627c4f341dc2cfc5a15f66db6a28b6
Deepak Singh Tomar <112108549+deepaktomar3352@users.noreply.github.com>
2024-07-05 15:09:30 +0530

    remove file from dashboard which was unnecessary

--- src/Admin/Pages/Dashboard/Dashboard.js
+++ src/Admin/Pages/Dashboard/Dashboard.js
@@ -17,11 +17,6 @@ import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-// import { mainListItems, secondaryListItems } from './listItems';
-// import Chart from './Chart';
-// import Deposits from './Deposits';
-// import Orders from './Orders';
-
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
