614d4c00064154ac66148a9d9505bc5961b77519
Rohittomar01 <rohittomar9285@gmail.com>
2024-07-05 20:13:33 +0530

    made category component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./Admin/Pages/Dashboard/Dashboard";
+import Category from "./Admin/Components/Category";
 
 
 function App() {
   return (
-    <div className="App">
-    <Dashboard/>
-    </div>
+    <Router className="App">
+      <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/category" element={<Category />} />
+    </Routes>
+    </Router>
   );
 }
 
